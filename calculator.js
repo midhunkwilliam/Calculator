6e6134edeebaad3135d49a2db0650f7a7cfa4e84
midhunkwilliam <midhunkwilliam@gmail.com>
2022-11-30 15:03:27 +0530

    calculator v1

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function displayvalue(num){
+  //result is id of
+  //input tag
+  result.value +=num}
+
+  function allClear(){
+    result.value=""
+  }
+
+  function evaluateOut(){
+    result.value=eval(result.value)
+  }
+
+  function back(){
+    result.value = result.value.slice(0,-1)
+  }
